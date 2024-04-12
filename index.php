fa5479ef7efb4b6897fc3fd4502cb841042da138
aniketbth <elitesam2003@gmail.com>
2024-04-12 16:37:51 +0530

    updated

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 include('config.php');
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Login V15</title>
+<title>Login V1</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -14,17 +16,11 @@ include('config.php');
 
 <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 
-<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-
 <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
 
 <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
 
-<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-
 <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-
-<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
 
 <link rel="stylesheet" type="text/css" href="css/util.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
@@ -35,93 +31,117 @@ include('config.php');
 <div class="limiter">
 <div class="container-login100">
 <div class="wrap-login100">
-<div class="login100-form-title" style="background-image: url(images/bg-01.jpg);">
-<span class="login100-form-title-1">
-Sign In
+<div class="login100-pic js-tilt" data-tilt>
+<img src="images/img-01.webp" alt="IMG">
+</div>
+
+
+<form class="login100-form validate-form" method="POST" enctype="multipart/form-data">
+<span class="login100-form-title">
+Member Login
+</span>
+
+
+<div class="wrap-input100 validate-input" data-validate="Name is Username">
+<input class="input100" type="text" name="username" placeholder="Username">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-envelope" aria-hidden="true"></i>
 </span>
 </div>
-<form class="login100-form validate-form" method="POST" enctype="multipart/form-data">
 
 
-<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
-<span class="label-input100">Username</span>
-<input class="input100" type="text" name="username" placeholder="Enter username">
+<div class="wrap-input100 validate-input" data-validate="Password is required">
+<input class="input100" type="password" name="password" placeholder="Password">
 <span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate="Password is required">
+<input class="input100" type="password" name="Cpassword" placeholder=" Confirm Password">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate="Email is required">
+<input class="input100" type="email" name="mail" placeholder="Email Address">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
 </div>
 
 
-<div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
-<span class="label-input100">Password</span>
-<input class="input100" type="password" name="pass" placeholder="Enter password">
+<div class="wrap-input100 validate-input" data-validate="DOB is required">
+<input class="input100" type="date" name="user_dob" placeholder="DOB">
 <span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate="conatct is required">
+<input class="input100" type="number" name="contact" placeholder="User Contact">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate="address is required">
+<input class="input100" type="text" name="user_address" placeholder="User Address">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
 </div>
 
 
-<div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
-<span class="label-input100">Password</span>
-<input class="input100" type="password" name="re_pass" placeholder="Enter password again">
+<div class="wrap-input100 validate-input" data-validate="image is required">
+<input class="input100" type="file" accept="image/jpg,image/jpeg,image/png" name="username_img" >
 <span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
 </div>
-
-
-<div class="wrap-input100 validate-input m-b-18" data-validate="Date is required">
-<span class="label-input100">DOB</span>
-<input class="input100" type="date" name="date" placeholder="Enter DOB">
-<span class="focus-input100"></span>
-</div>
-
-
-<div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
-<span class="label-input100">Conatct no.</span>
-<input class="input100" type="password" name="Cnumber" placeholder="Enter contact no.">
-<span class="focus-input100"></span>
-</div>
-
-
-<div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
-<span class="label-input100">Email-Id</span>
-<input class="input100" type="email" name="email" placeholder="Enter email">
-<span class="focus-input100"></span>
-</div>
-
 
 <?php 
 
-$textGenerator = '1234567890USERCODE';
+$UserCode = '1234567890USERCODE';
 
-$code= substr(str_shuffle($textGenerator),0,8);
+$code= substr(str_shuffle($UserCode),0,8);
 
 
 ?> 
 
 
-<div class="wrap-input100 validate-input m-b-18" >
-<span class="label-input100">Unique Code</span>
-<input class="input100" type="text" readonly="" name="Ucode" value="<?php echo $code;?>" >
+<div class="wrap-input100 validate-input">
+<input class="input100" readonly="" type="text" name="e_code" value="<?php echo $code;?>">
 <span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
 </div>
 
-<div class="wrap-input100 validate-input m-b-18" data-validate="Address is required">
-<span class="label-input100">Address</span>
-<input class="input100" type="address" name="Uaddress" placeholder="Enter Address">
+<div class="wrap-input100 validate-input" data-validate="address is required">
+<input class="input100" type="text" name="Scode" placeholder="State Code">
 <span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
 </div>
 
-
-<div class="wrap-input100 validate-input m-b-18" data-validate="Image is required">
-<span class="label-input100">User Image</span>
-<input class="input100" type="file" name="image" >
+<div class="wrap-input100 validate-input" data-validate="address is required">
+<input class="input100" type="text" name="Ccode" placeholder="Country Code">
 <span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
 </div>
-
-
-<div class="wrap-input100 validate-input m-b-18" data-validate="State Code is required">
-<span class="label-input100">State Code</span>
-<input class="input100" type="text" name="Uaddress" placeholder="Enter State Code">
-<span class="focus-input100"></span>
-</div>
-
-
 
 
 <?php 
@@ -134,78 +154,82 @@ $otp = substr(str_shuffle($OTPgenerator),0,6);
 ?>
 
 
-<div class="wrap-input100 validate-input m-b-18" >
-<span class="label-input100">OTP</span>
-<input class="input100" type="text" readonly="" name="otp" value="<?php echo $otp;?>">
+<div class="wrap-input100 validate-input">
+<input class="input100" readonly="" type="text" name="otp" value="<?php echo $otp;?>">
 <span class="focus-input100"></span>
-</div>
-
-
-
-<div class="wrap-input100 validate-input m-b-18" data-validate="Country Code is required">
-<span class="label-input100">Country Code</span>
-<input class="input100" type="text" name="Uaddress" placeholder="Enter Country Code">
-<span class="focus-input100"></span>
-</div>
-
-<div>
-<a href="#" class="txt1">
-Forgot Password?
-</a>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
 </div>
 
 <div class="container-login100-form-btn">
-<button class="login100-form-btn" name="signup" type="submit">
+<button class="login100-form-btn" type="submit" name="submit">
 Login
 </button>
 </div>
+
+
+<div class="text-center p-t-12">
+<span class="txt1">
+Forgot
+</span>
+<a class="txt2" href="#">
+Username / Password?
+</a>
+</div>
+<div class="text-center p-t-136">
+<a class="txt2" href="#">
+Create your Account
+<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+</a>
+</div>
+
 <?php 
 
-    $username = $_POST['username'];
-    $password = $_POST['pass'];
-    $cnfpassword = $_POST['re_pass'];
-    $DOB = $_POST['date'];
-    $contact = $_POST['Cnumber'];
-    $mail = $_POST['email'];
-    $UserCode = $_POST['Ucode'];
-    $Address = $_POST['Uaddress'];
-    $Scode = $_POST['Scode'];
-    $Ccode = $_POST['Ccode'];
-    $UserOtp = $_POST['otp'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$Cpassword = $_POST['Cpassword'];
+$email = $_POST['mail'];
+$DOB = $_POST['user_dob'];
+$contact = $_POST['contact'];
+$addres = $_POST['user_address'];
+$U_code = $_POST['e_code'];
+$Scode = $_POST['Scode'];
+$Ccode = $_POST['Ccode'];
+$otp = $_POST['otp'];
+
 
 $targetFolder = 'userimage/';
-$base_address = 'http://localhost/registration_data/';
-$orgFileName = $_FILES['image']['name'];
-$tmpFileName = $_FILES['image']['tmp_name'];
+$base_address = 'http://localhost/login/';
+$orgFileName = $_FILES['username_img']['name'];
+$tmpFileName = $_FILES['username_img']['tmp_name'];
 
 $completeAddress = $base_address.$targetFolder.$orgFileName;
 
-if(isset($_POST['signup']))
+if(isset($_POST['submit']))
 {
-      session_start();
-    $_SESSION['activeUser'] = $username;
-    move_uploaded_file($tmpFileName,$targetFolder.$orgFileName);
+  move_uploaded_file($tmpFileName,$targetFolder.$orgFileName);
 
-    if($password == $cnfpassword)
-    {
-    echo "<script>window.location.href='otpverify.php'</script>";
+  if($password==$Cpassword)
+  {
+       $insertData = mysqli_query($config,"INSERT INTO sign_up(username,password,user_dob,contact_no,user_mail,user_otp,otp_status,address,employee_code,employee_img,state_code,country_code) VALUES('$username','$password','$DOB','$contact','$email','$otp','Pending','$addres','$U_code','$completeAddress','$Scode','$Ccode')"); 
+       if($insertData)
+       {
+        session_start();
+       $_SESSION['loggedInEMP'] = $email;
+       echo "<script>window.location.href='dashboard.php'</script>";
+        
+       }
+       else
+       {
+        echo "<script>alert('Try again')</script>";
+       }
+   }
 
-     $insertData = mysqli_query($config,"INSERT INTO sign_up(username,password,user_dob,contact_no,user_mail,user_otp,otp_status,address,employee_code,employee_img,state_code,country_code) VALUES('$username','$password','$DOB','$contact','$mail','$UserOtp','Pending','$Address','$UserCode','$completeAddress','$Scode','$Ccode')"); 
+}
 
-       
-
-     if($insertData)
-     {
-        echo "<script>alert('Data and User Image Inserted')</script>";
-        echo "<script>window.location.href='otpverify.php'</script>";
-     }
-     else
-     {
-      echo "<script>alert('Try again')</script>";
-     }
-    }
-  }
 ?>
+
 
 </form>
 </div>
@@ -214,21 +238,18 @@ if(isset($_POST['signup']))
 
 <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 
-<script src="vendor/animsition/js/animsition.min.js"></script>
-
 <script src="vendor/bootstrap/js/popper.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
 <script src="vendor/select2/select2.min.js"></script>
 
-<script src="vendor/daterangepicker/moment.min.js"></script>
-<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<script src="vendor/tilt/tilt.jquery.min.js"></script>
 
-<script src="vendor/countdowntime/countdowntime.js"></script>
-
-<script src="js/main.js"></script>
 
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
 
+
+<script src="js/main.js"></script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v84a3a4012de94ce1a686ba8c167c359c1696973893317" integrity="sha512-euoFGowhlaLqXsPWQ48qSkBSCFs3DPRyiwVu3FjR96cMPx+Fr+gpWRhIafcHwqwCqWS42RZhIudOvEI+Ckf6MA==" data-cf-beacon='{"rayId":"8731530fab5147de","version":"2024.3.0","token":"cd0b4b3a733644fc843ef0b185f98241"}' crossorigin="anonymous"></script>
 </body>
 </html>
